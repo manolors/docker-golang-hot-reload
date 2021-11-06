60a8f8ada6b24766fba0cced4639a8be8656159e
Manuel Rodríguez Santana <lolo@turitop.com>
2021-11-06 13:15:45 +0000

    Add hot reload data

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/manolors/docker-golang-hot-reload
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Testeando AIR para hot reload con Go")
+}
